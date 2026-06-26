1f993d0d2899e162489d9cb7cf20e5b9d6644bd5
mocelincea-source <mocelincea@gmail.com>
2026-06-26 12:20:56 -0300

    fix: 3 pequenos ajustes e refinamentos

--- .vercel/output/functions/__server.func/_ssr/index.mjs
+++ .vercel/output/functions/__server.func/_ssr/index.mjs
@@ -81,7 +81,7 @@ function getResponse() {
 }
 var HEADERS = { TSS_SHELL: "X-TSS_SHELL" };
 async function getStartManifest(matchedRoutes) {
-  const { tsrStartManifest } = await import("../_tanstack-start-manifest_v-CkR-udbZ.mjs");
+  const { tsrStartManifest } = await import("../_tanstack-start-manifest_v-eAH3Ty5r.mjs");
   const startManifest = tsrStartManifest();
   const rootRoute = startManifest.routes[rootRouteId] = startManifest.routes[rootRouteId] || {};
   rootRoute.assets = rootRoute.assets || [];
@@ -683,7 +683,7 @@ var baseManifestPromise;
 var cachedFinalManifestPromise;
 async function loadEntries() {
   const [routerEntry, startEntry, pluginAdapters] = await Promise.all([
-    import("./router-BNNZhwE6.mjs").then((n) => n.r),
+    import("./router-BN0x4rgk.mjs").then((n) => n.r),
     import("./start-HYkvq4Ni.mjs"),
     import("../__23tanstack-start-plugin-adapters-Cwee5PKy.mjs")
   ]);
