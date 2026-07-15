5528c6ca8b6ef79dcf32d17ff2c5ee0285173be6
mocelincea-source <mocelincea@gmail.com>
2026-07-15 03:08:22 -0300

    feat(investidores): adiciona pdfs estaticos e finaliza mapeamento de urls nas paginas de RI

--- src/routes/investidores/outros-documentos.tsx
+++ src/routes/investidores/outros-documentos.tsx
@@ -36,22 +36,22 @@ const SECTIONS: CompanySection[] = [
           {
             title: "Escritura de Emissão",
             date: "30/01/2026",
-            href: "",
+            href: "/documentos/outros-documentos/Éden Empreendimentos e Participações S.A/Emissão de Debêntures/projeto_eden_escritura_de_emissao_v._assinada55642149.1.pdf",
           },
           {
             title: "Escritura de Emissão - 1º aditamento",
             date: "30/01/2026",
-            href: "",
+            href: "/documentos/outros-documentos/Éden Empreendimentos e Participações S.A/Emissão de Debêntures/projeto_eden_1o_aditamento_escritura_de_emissao_v.assinada.pdf",
           },
           {
             title: "Anúncio de Encerramento",
             date: "30/01/2026",
-            href: "",
+            href: "/documentos/outros-documentos/Éden Empreendimentos e Participações S.A/Emissão de Debêntures/anuncio_de_encerramento.pdf",
           },
           {
             title: "Anúncio de Início",
             date: "29/01/2026",
-            href: "",
+            href: "/documentos/outros-documentos/Éden Empreendimentos e Participações S.A/Emissão de Debêntures/projeto_eden_anuncio_de_inicio_v._final_pdf55705034.1.pdf",
           },
         ],
       },
@@ -70,12 +70,12 @@ const SECTIONS: CompanySection[] = [
           {
             title: "Edital de Convocação AGD - 12/02/2026",
             date: "11/02/2026",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/AGD 2ª Emissão - MGSP12/Edital de Convocação AGD - 12_02_2026.pdf",
           },
           {
             title: "Instrução de voto à distância AGD - 12/02/2026",
             date: "11/02/2026",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/AGD 2ª Emissão - MGSP12/Instrução de voto à distância  AGD - 12_02_2026.pdf",
           },
         ],
       },
@@ -86,12 +86,12 @@ const SECTIONS: CompanySection[] = [
           {
             title: "Edital de Convocação AGD - 12/02/2026",
             date: "11/02/2026",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/AGD 4ª Emissão - MGSP14/Edital de Convocação AGD - 12_02_2026.pdf",
           },
           {
             title: "Instrução de voto à distância AGD - 12/02/2026",
             date: "11/02/2026",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/AGD 4ª Emissão - MGSP14/Instrução de voto à distância  AGD - 12_02_2026.pdf",
           },
         ],
       },
@@ -103,45 +103,45 @@ const SECTIONS: CompanySection[] = [
           {
             title: "Anúncio de Encerramento",
             date: "26/02/2026",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/Emissão de Debentures - 5ª Emissão/Anúncio de Encerramento.pdf",
           },
           {
             title: "Anúncio de Início",
             date: "25/02/2026",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/Emissão de Debentures - 5ª Emissão/Anúncio de Início.pdf",
           },
           {
             // "2o" preservado do legado (sem ordinal feminino/masculino)
             title: "2o Aditamento à Escritura de Emissão",
             date: "25/02/2026",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/Emissão de Debentures - 5ª Emissão/2o Aditamento à Escritura de Emissão.pdf",
           },
           {
             title: "Comunicado ao Mercado",
             date: "25/02/2026",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/Emissão de Debentures - 5ª Emissão/Comunicado ao Mercado.pdf",
           },
           {
             title: "Comunicado ao Mercado",
             date: "24/02/2026",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/Emissão de Debentures - 5ª Emissão/Comunicado ao Mercado2.pdf",
           },
           {
             title: "Aviso ao Mercado",
             date: "19/02/2026",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/Emissão de Debentures - 5ª Emissão/Aviso ao Mercado.pdf",
           },
           {
             // "1o" preservado do legado
             title: "1o Aditamento à Escritura de Emissão",
             date: "13/02/2026",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/Emissão de Debentures - 5ª Emissão/1o Aditamento à Escritura de Emissão.pdf",
           },
           {
             // "5a" preservado do legado
             title: "Escritura de Emissão - 5a Emissão",
             date: "19/12/2025",
-            href: "",
+            href: "/documentos/outros-documentos/TRANSMISSORA SP -MG S.A/Emissão de Debentures - 5ª Emissão/Escritura de Emissão - 5a Emissão .pdf",
           },
         ],
       },
@@ -159,7 +159,7 @@ const SECTIONS: CompanySection[] = [
           {
             title: "Edital de Convocação AGD - 02/06/2023",
             date: "02/06/2023",
-            href: "",
+            href: "/documentos/outros-documentos/HRZ ES - Horizon Transmissão ES S.A/AGD/Edital de Convocação AGD - 02_06_2023.pdf",
           },
         ],
       },
@@ -170,22 +170,22 @@ const SECTIONS: CompanySection[] = [
           {
             title: "ICSD 2025 - HRZ ES",
             date: "31/12/2025",
-            href: "",
+            href: "/documentos/outros-documentos/HRZ ES - Horizon Transmissão ES S.A/ICSD/ICSD 2025 - HRZ ES.pdf",
           },
           {
             title: "ICSD 2024 - HRZ ES",
             date: "31/12/2024",
-            href: "",
+            href: "/documentos/outros-documentos/HRZ ES - Horizon Transmissão ES S.A/ICSD/ICSD 2024 - HRZ ES.pdf",
           },
           {
             title: "ICSD 2023 - HRZ ES",
             date: "31/12/2023",
-            href: "",
+            href: "/documentos/outros-documentos/HRZ ES - Horizon Transmissão ES S.A/ICSD/ICSD 2023 - HRZ ES.pdf",
           },
           {
             title: "ICSD 2022 - HRZ ES",
             date: "31/12/2022",
-            href: "",
+            href: "/documentos/outros-documentos/HRZ ES - Horizon Transmissão ES S.A/ICSD/ICSD 2022 - HRZ ES.pdf",
           },
         ],
       },
@@ -204,7 +204,7 @@ const SECTIONS: CompanySection[] = [
             // Caixa alta preservada do legado
             title: "ICSD 2025 - HRZ TRANSMISSÃO E PARTICIPAÇÕES S.A.",
             date: "31/12/2025",
-            href: "",
+            href: "/documentos/outros-documentos/HRZ Transmissão e Participações S.A/ICSD/ICSD 2025 - HRZ TRANSMISSÃO E PARTICIPAÇÕES S.A..pdf",
           },
         ],
       },
@@ -222,17 +222,17 @@ const SECTIONS: CompanySection[] = [
           {
             title: "Anúncio de Encerramento",
             date: "30/04/2025",
-            href: "",
+            href: "/documentos/outros-documentos/Edge Empreendimentos e Participações S.A/Emissão de Debêntures/Anúncio de Encerramento.pdf",
           },
           {
             title: "Comunicado ao Mercado",
             date: "28/04/2025",
-            href: "",
+            href: "/documentos/outros-documentos/Edge Empreendimentos e Participações S.A/Emissão de Debêntures/Comunicado ao Mercado.pdf",
           },
           {
             title: "Anúncio de Início",
             date: "23/04/2025",
-            href: "",
+            href: "/documentos/outros-documentos/Edge Empreendimentos e Participações S.A/Emissão de Debêntures/Anúncio de Início.pdf",
           },
         ],
       },
@@ -250,12 +250,12 @@ const SECTIONS: CompanySection[] = [
           {
             title: "Anúncio de Encerramento",
             date: "29/05/2025",
-            href: "",
+            href: "/documentos/outros-documentos/Victory Empreendimentos e Participações S.A/Emissão de Debêntures/Anúncio de Encerramento.pdf",
           },
           {
             title: "Anúncio de Início",
             date: "22/05/2025",
-            href: "",
+            href: "/documentos/outros-documentos/Victory Empreendimentos e Participações S.A/Emissão de Debêntures/Anúncio de Início.pdf",
           },
         ],
       },
